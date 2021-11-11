dff2b6b748f217aca9288a4c000013e14fd042db
Kristine Sundt Lorentzen <kristine.sundt.lorentzen@gmail.com>
2021-11-11 22:46:18 +0100

    perf: remove unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,4 @@
-import {
-  amber,
-  blueGrey,
-  cyan,
-  deepOrange,
-  green,
-  teal,
-} from '@mui/material/colors';
+import { blueGrey, deepOrange } from '@mui/material/colors';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
