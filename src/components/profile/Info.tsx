e3c955a5359e378e4ca5aa8de56b9ba3eca5a5bf
Kristine Sundt Lorentzen <kristine.sundt.lorentzen@gmail.com>
2021-11-15 21:41:14 +0100

    style: change icon for phone number

--- src/components/profile/Info.tsx
+++ src/components/profile/Info.tsx
@@ -1,4 +1,6 @@
-import { Email, Phone, Place } from '@emotion-icons/material-outlined';
+import {
+  Email, PhoneIphone, Place
+} from '@emotion-icons/material-outlined';
 import { Icon, Typography } from '@mui/material';
 import { Box } from '@mui/system';
 import { FormattedMessage } from 'react-intl';
@@ -27,7 +29,7 @@ export const Info = () => {
         </Typography>
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <Icon component={Phone} sx={{ mr: 2, color: 'text.disabled' }}></Icon>
+        <Icon component={PhoneIphone} sx={{ mr: 2, color: 'text.disabled' }}></Icon>
         <Typography variant="body1">+47 932 16 768</Typography>
       </Box>
     </Box>
