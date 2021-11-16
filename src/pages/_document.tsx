94e7a4e49e61e47ccc6f82ec4f34565dbf95b719
Kristine Sundt Lorentzen <kristine.sundt.lorentzen@gmail.com>
2021-11-16 02:34:44 +0100

    bug: fix favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,19 +8,19 @@ class Document extends BaseDocument {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/favicons/apple-touch-icon.png"
+            href="/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicons/favicon-32x32.png"
+            href="/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicons/favicon-16x16.png"
+            href="/favicon-16x16.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
           <link
