018f1aadb9199104e9286c77cd5586449dcbc277
Kristine Sundt Lorentzen <kristine.sundt.lorentzen@gmail.com>
2021-11-16 14:55:24 +0100

    style: fix styling for list

--- src/components/categories/LookingFor.tsx
+++ src/components/categories/LookingFor.tsx
@@ -1,15 +1,38 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import { useTheme } from '@mui/system';
 import { FormattedMessage } from 'react-intl';
 import data from '../../../assets/data/lookingFor.json';
 
-export const LookingFor = () => (
-  <ul>
-    {data.points.map((item, index) => (
-      <li key={index}>
-        <Typography variant="body2">
-          <FormattedMessage id={item} />
-        </Typography>
-      </li>
-    ))}
-  </ul>
-);
+export const LookingFor = () => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      component="ul"
+      sx={{
+        margin: 0,
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 1,
+        pl: 3.5,
+      }}
+    >
+      {data.points.map((item, index) => (
+        <Box
+          component="li"
+          key={index}
+          sx={{
+            '&::marker': {
+              color: theme.palette.secondary.main,
+              fontSize: '0.8rem',
+            },
+          }}
+        >
+          <Typography variant="body2">
+            <FormattedMessage id={item} />
+          </Typography>
+        </Box>
+      ))}
+    </Box>
+  );
+};
