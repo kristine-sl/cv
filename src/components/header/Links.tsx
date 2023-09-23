47be4aea313a6697a35b7532633fae01d1e99b9d
Kristine Sundt Lorentzen <kristine.sundt.lorentzen@gmail.com>
2023-09-23 23:51:38 +0200

    fix: add suppressHydrationWarning to button

--- src/components/header/Links.tsx
+++ src/components/header/Links.tsx
@@ -10,17 +10,32 @@ interface LinksProps {
 export const Links = ({ github, linkedIn, instagram }: LinksProps) => (
   <>
     {linkedIn && (
-      <IconButton href={linkedIn} color="secondary" aria-label="LinkedIn">
+      <IconButton
+        href={linkedIn}
+        color="secondary"
+        aria-label="LinkedIn"
+        suppressHydrationWarning
+      >
         <Icon component={Linkedin} suppressHydrationWarning />
       </IconButton>
     )}
     {github && (
-      <IconButton href={github} color="secondary" aria-label="Github">
+      <IconButton
+        href={github}
+        color="secondary"
+        aria-label="Github"
+        suppressHydrationWarning
+      >
         <Icon component={Github} suppressHydrationWarning />
       </IconButton>
     )}
     {instagram && (
-      <IconButton href={instagram} color="secondary" aria-label="Instagram">
+      <IconButton
+        href={instagram}
+        color="secondary"
+        aria-label="Instagram"
+        suppressHydrationWarning
+      >
         <Icon component={Instagram} suppressHydrationWarning />
       </IconButton>
     )}
