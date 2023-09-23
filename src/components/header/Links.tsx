1a997cb80ae2d492c63efc03560e5b2e6d5d814b
Kristine Sundt Lorentzen <kristine.sundt.lorentzen@gmail.com>
2023-09-23 23:18:05 +0200

    fix: more ghost hunting

--- src/components/header/Links.tsx
+++ src/components/header/Links.tsx
@@ -9,6 +9,11 @@ interface LinksProps {
 
 export const Links = ({ github, linkedIn, instagram }: LinksProps) => (
   <>
+    {instagram && (
+      <IconButton href={instagram} color="secondary" aria-label="Instagram">
+        <Icon component={Instagram} />
+      </IconButton>
+    )}
     {linkedIn && (
       <IconButton href={linkedIn} color="secondary" aria-label="LinkedIn">
         <Icon component={Linkedin} />
@@ -17,11 +22,6 @@ export const Links = ({ github, linkedIn, instagram }: LinksProps) => (
     {github && (
       <IconButton href={github} color="secondary" aria-label="Github">
         <Icon component={Github} />
-      </IconButton>
-    )}
-    {instagram && (
-      <IconButton href={instagram} color="secondary" aria-label="Instagram">
-        <Icon component={Instagram} />
       </IconButton>
     )}
   </>
